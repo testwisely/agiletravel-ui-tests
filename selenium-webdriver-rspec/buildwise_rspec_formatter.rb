f59e5f8ca83989f994cc3713171b0a44de661527
Zhimin Zhan <zhimin@itest2.com>
2017-09-16 08:36:57 +1000

    fixed rspec integration

--- selenium-webdriver-rspec/buildwise_rspec_formatter.rb
+++ selenium-webdriver-rspec/buildwise_rspec_formatter.rb
@@ -27,7 +27,9 @@ module CI::Reporter
       	if $browser && $browser.respond_to?("save_screenshot") # shall be set in test script           
       	   reports_dir  =  ENV['CI_REPORTS'] || File.expand_path("#{Dir.getwd}/spec/reports")
            screenshots_dir = File.join(reports_dir, "screenshots")
-           spec_file_name = File.basename(notification.example_name.path)
+           FileUtils.mkdir_p(screenshots_dir) unless File.exists?(screenshots_dir)
+           
+           spec_file_name = File.basename(notification.example.full_path)
            saved_to = File.join(screenshots_dir, spec_file_name)
             
            FileUtils.mkdir_p(saved_to) unless File.exists?(saved_to)
