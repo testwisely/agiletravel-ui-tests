5c7cc9802c8900ebc8f686153ffc9aab94a80344
Zhimin Zhan <zhimin@Zhimins-iMac.local>
2018-09-03 12:00:53 +1000

    remove rwebspec framework examples, just focus on selneium webdriver

--- rwebspec/pages/abstract_page.rb
+++
@@ -1,9 +0,0 @@
-# This is the parent page for all page objects, for operations across all pages, define here
-class AbstractPage < RWebSpec::AbstractWebPage
-
-  def initialize(driver, text = "")
-    super(driver, text)	
-  end
-
-  
-end
