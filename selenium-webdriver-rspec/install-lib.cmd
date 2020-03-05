4f5840666c4444c337d489cbe3bfd2cd22d7f780
Zhimin Zhan <zhimin@itest2.com>
2020-03-05 14:12:33 +1000

    add notes for Ruby 2.6

--- selenium-webdriver-rspec/install-lib.cmd
+++ selenium-webdriver-rspec/install-lib.cmd
@@ -1,1 +1,2 @@
-gem install rspec selenium-webdriver ci_reporter_rspec
+# For Ruby 2.5 or less, replace --no-document with --no-ri --no-rdoc
+gem install --no-document rspec selenium-webdriver ci_reporter_rspec
