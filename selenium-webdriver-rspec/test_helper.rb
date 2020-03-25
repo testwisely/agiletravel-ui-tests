292f938933f0730f430b721f2ff7d354bcd221df
Zhimin Zhan <zhimin@itest2.com>
2020-03-25 20:25:47 +1000

    set if debugging mode, enable debugging port for chromedriver

--- selenium-webdriver-rspec/test_helper.rb
+++ selenium-webdriver-rspec/test_helper.rb
@@ -56,7 +56,7 @@ module TestHelper
         the_chrome_options.add_argument("--headless")
       end
 
-      if defined?(TestWiseRuntimeSupport)
+      if defined?(TestWiseRuntimeSupport) && debugging?
         browser_debugging_port = get_browser_debugging_port() rescue 19218 # default port
         puts("Enabled chrome browser debug port: #{browser_debugging_port}")
         the_chrome_options.add_argument("--remote-debugging-port=#{browser_debugging_port}")
