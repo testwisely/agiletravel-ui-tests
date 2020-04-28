43c63671d9e16710c7cb2b3b55ac7ea82e47f4cd
Zhimin Zhan <zhimin@itest2.com>
2020-04-28 16:49:19 +1000

    fixed test

--- selenium-webdriver-rspec/spec/03_passenger_spec.rb
+++ selenium-webdriver-rspec/spec/03_passenger_spec.rb
@@ -40,7 +40,7 @@ describe "Passenger" do
     passenger_page.click_next
 
     # the step below fails: "Wendy" => "Bob"
-    expect(driver.find_element(:name, "holder_name").attribute("value")).to eq("Wendy Tester")
+    expect(driver.find_element(:name, "holder_name").attribute("value")).to eq("Bob Tester")
   end
 
 end
