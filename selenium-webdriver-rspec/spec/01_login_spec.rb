a428da699e50c3f35affdf7704f20d6026836bca
zhimin zhan <zhimin@itest2.com>
2020-07-09 07:15:07 +1000

    add deubg log selenium usage

--- selenium-webdriver-rspec/spec/01_login_spec.rb
+++ selenium-webdriver-rspec/spec/01_login_spec.rb
@@ -1,5 +1,10 @@
 load File.dirname(__FILE__) + '/../test_helper.rb'
 
+# don't use selenium excessive log directly when running in TestWise
+# if you want do, use the statement below, and run the test from command line to see debug log
+#
+# Selenium::WebDriver.logger.level = :debug unless defined?(TestWiseRuntimeSupport)
+
 describe "User Login" do
   include TestHelper
 
@@ -7,7 +12,7 @@ describe "User Login" do
     # for windows, when unable auto-detect firefox binary
     # Please note Firefox on 32 bit is "C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
     # Selenium::WebDriver::Firefox::Binary.path="C:/Program Files/Mozilla Firefox/firefox.exe"
-      
+    
     @driver = $driver = Selenium::WebDriver.for(browser_type, browser_options)
     driver.manage().window().resize_to(1280, 720)
     driver.manage().window().move_to(30, 78)
@@ -22,8 +27,9 @@ describe "User Login" do
 
   it "[1] Can sign in OK" do
     goto_page("/login")
-    login_page = LoginPage.new(driver)
-    login_page.login("agileway", "testwise")  
+    driver.find_element(:id, "username").send_keys("agileway")
+    driver.find_element(:id, "password").send_keys("testwise")
+    driver.find_element(:name, "commit").click
     # selenium does not have browser.text yet
     try_for(3) {  expect(driver.page_source).to include("Welcome")}
     driver.find_element(:link_text, "Sign off").click
