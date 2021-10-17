27f1776e740c245653a71fa2a2a37076d01b9be2
Courtney Zhan <qinmengzhan@gmail.com>
2021-10-17 19:18:52 +1000

    update pakcage

--- selenium-webdriver-rspec/spec/login_spec.rb
+++ selenium-webdriver-rspec/spec/login_spec.rb
@@ -1,4 +1,4 @@
-load File.dirname(__FILE__) + '/../test_helper.rb'
+load File.dirname(__FILE__) + "/../test_helper.rb"
 
 # don't use selenium excessive log directly when running in TestWise
 # if you want do, use the statement below, and run the test from command line to see debug log
@@ -12,11 +12,11 @@ describe "User Login" do
     # for windows, when unable auto-detect firefox binary
     # Please note Firefox on 32 bit is "C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
     # Selenium::WebDriver::Firefox::Binary.path="C:/Program Files/Mozilla Firefox/firefox.exe"
-    
+
     @driver = $driver = Selenium::WebDriver.for(browser_type, browser_options)
     driver.manage().window().resize_to(1280, 720)
     driver.manage().window().move_to(30, 78)
-    driver.get(site_url)    
+    driver.get(site_url)
   end
 
   after(:all) do
@@ -40,5 +40,4 @@ describe "User Login" do
     login_page.login("agileway", "badpass")
     expect(driver.page_source).to include("Invalid email or password")
   end
-
 end
